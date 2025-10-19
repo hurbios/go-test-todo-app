f1fe3088ee43104ef45c9c757ed5ff90854ff396
hurbios <15719468+hurbios@users.noreply.github.com>
2025-10-19 23:08:11 +0300

    Add single todo getter route

--- resources/mock_todos.go
+++ resources/mock_todos.go
@@ -1,20 +1,28 @@
 package resources
 
 type Todo struct {
+	Id        int    `json:"id"`
 	Title     string `json:"title"`
 	Body      string `json:"body"`
 	CreatedAt string `json:"createdAt"`
-	Owner     uint   `json:"owner"`
+	Owner     int    `json:"owner"`
 }
 
 func GetAllTodos() []Todo {
-	atodo := Todo{
+	var todos []Todo
+	todos = append(todos, Todo{
+		Id:        1,
 		Title:     "first note",
 		Body:      "This is the first note for testing purposes.",
 		CreatedAt: "2025-10-19",
 		Owner:     1,
-	}
-	var todos []Todo
-	todos = append(todos, atodo)
+	})
+	todos = append(todos, Todo{
+		Id:        2,
+		Title:     "second note",
+		Body:      "Just another testnote...",
+		CreatedAt: "2025-10-19",
+		Owner:     1,
+	})
 	return todos
 }
--- todo_router.go
+++ todo_router.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"test-todo-app/resources"
 
 	"github.com/gorilla/mux"
@@ -12,6 +13,25 @@ func AllTodos(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(resources.GetAllTodos())
 }
 
+func OneTodo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Bad id", http.StatusBadRequest)
+		return
+	}
+	alltodos := resources.GetAllTodos()
+
+	for i := 0; i < len(alltodos); i++ {
+		if alltodos[i].Id == id {
+			json.NewEncoder(w).Encode(alltodos[i])
+			return
+		}
+	}
+	http.Error(w, "Id not found", http.StatusNotFound)
+}
+
 func TodoRouter(todorouter *mux.Router) {
 	todorouter.HandleFunc("/", AllTodos).Methods("GET")
+	todorouter.HandleFunc("/{id}", OneTodo).Methods("GET")
 }
