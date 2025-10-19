8a716da2fe071cbc770acf9d9f8e0007a56730fd
hurbios <15719468+hurbios@users.noreply.github.com>
2025-10-19 22:21:30 +0300

    Add static server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Good evening!")
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/", fs)
+	http.Handle("/night", fs)
+
+	http.ListenAndServe(":80", nil)
 }
